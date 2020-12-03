78339f551fba876e86008278c09a5fb465703686
Amy Legge <amylegge@Amys-MacBook-Pro.local>
2020-12-03 16:03:58 -0500

    fixed error in hashing password

--- models/User.js
+++ models/User.js
@@ -50,8 +50,7 @@ User.init(
                     return updatedUserData;
                 }
             },
-        },
-        {
+
         sequelize,
         timestamps: false,
         freezeTableName: true,
